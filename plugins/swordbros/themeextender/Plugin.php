c00ad1fdeaa62b131128cfe7f8e297f137575fc0
Anastasia Zhukova <7880553@gmail.com>
2024-07-11 17:12:31 +0200

    build menu with tailor nesteditems

--- plugins/swordbros/themeextender/Plugin.php
+++ plugins/swordbros/themeextender/Plugin.php
@@ -2,23 +2,17 @@
 
 namespace Swordbros\ThemeExtender;
 
-use App;
-use Backend\Models\BrandSetting;
-use Config;
 use Event;
 use Swordbros\ThemeExtender\Components\Breadcrumb;
 use Swordbros\ThemeExtender\Classes\AddEssentialsVars;
-use Swordbros\ThemeExtender\Components\Menu;
 use Swordbros\ThemeExtender\Classes\ExtendBoxesPage;
 use Swordbros\ThemeExtender\Classes\ExtendBoxesPagesWithSlug;
 use Swordbros\ThemeExtender\Classes\ExtendBoxesScaffoldingClasses;
-use Swordbros\ThemeExtender\Classes\MenuCacheDeleteHandler;
 use System\Classes\PluginBase;
-use System\Helpers\View;
 
 class Plugin extends PluginBase
 {
-    public $require = ['RainLab.Pages', 'OFFLINE.Boxes'];
+    public $require = ['OFFLINE.Boxes'];
 
     public function pluginDetails()
     {
@@ -39,25 +33,9 @@ class Plugin extends PluginBase
     {
         Event::subscribe(ExtendBoxesPagesWithSlug::class);
         Event::subscribe(ExtendBoxesPage::class);
-        Event::subscribe(MenuCacheDeleteHandler::class);
         Event::subscribe(ExtendBoxesScaffoldingClasses::class);
         Event::subscribe(AddEssentialsVars::class);
 
-        /*        Event::listen('backend.menu.extendItems', function(NavigationManager $navigationManager) {
-            $user = \BackendAuth::getUser(); // get the logged in user
-           $navigationManager->removeMainMenuItem('Rainlab.Pages', 'pages');
-            $navigationManager->addMainMenuItem('Rainlab.Pages', 'pages',
-            [
-                'label'       => 'rainlab.pages::lang.plugin.name',
-                'url'         => Backend::url('rainlab/pages#menu'),
-                'icon'        => 'icon-files-o',
-                'attributes'  => ['data-menu-item'=>'menus'],
-                'iconSvg'     => 'plugins/rainlab/pages/assets/images/pages-icon.svg',
-                'permissions' => ['rainlab.pages.*'],
-                'order'       => 200,
-                'useDropdown' => false,
-            ]);
-        });*/
         /**
          * Adds custom classes only to editor in backend
          */
@@ -73,7 +51,6 @@ class Plugin extends PluginBase
     public function registerComponents()
     {
         return [
-            Menu::class => 'boxesMenu',
             Breadcrumb::class => 'breadCrumb',
         ];
     }
