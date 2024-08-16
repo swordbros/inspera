245aeead52fc4134bd9b460838ed5b0f748cb897
anastasiauz <7880553@gmail.com>
2024-08-16 11:27:58 +0200

    items-slider, 50text50pic

--- themes/inspera/assets/js/scripts.js
+++ themes/inspera/assets/js/scripts.js
@@ -143,26 +143,26 @@
   });
 
 
-  // IMAGE BOX CAROUSEL
-  // var swiper = new Swiper('.image-box-carousel', {
-  //   slidesPerView: 1,
-  //   loop: true,
-  //   spaceBetween: 0,
-  //   breakpoints: {
-  //     640: {
-  //       slidesPerView: 1,
-  //       spaceBetween: 0,
-  //     },
-  //     768: {
-  //       slidesPerView: 2,
-  //       spaceBetween: 30,
-  //     },
-  //     1024: {
-  //       slidesPerView: 3,
-  //       spaceBetween: 60,
-  //     },
-  //   }
-  // });
+  //IMAGE BOX CAROUSEL
+  var swiper = new Swiper('.image-box-carousel', {
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 0,
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 60,
+      },
+    }
+  });
 
 
   // HOME INTRO SLIDER
