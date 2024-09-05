40afa6fe2f2a40849c9a929064d35f02654ca34b
tulparstudyo <teknovipyazilim@gmail.com>
2024-09-06 00:52:32 +0300

    Update _site_id.php

--- plugins/swordbros/booking/models/bookingmodel/_site_id.php
+++ plugins/swordbros/booking/models/bookingmodel/_site_id.php
@@ -6,8 +6,4 @@ if($record->is_new){
 }
 $site = \Site::getSiteFromId($record->site_id);
 if ($site) {
-
-    echo  '<span class="event-translate nav-icon nav-icon-flag" ><i class="'.$site->flagIcon.'"></i></span>'; // Dizi olarak döner
-}
-
-
+    echo  '<span class="event-translate nav-icon nav-icon-flag" ><i class="'.$site->flagIcon.'"></i></span>';
