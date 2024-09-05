6f8338d45b81afb9f71ebd1c229e1ca6e9fdffa6
tulparstudyo <teknovipyazilim@gmail.com>
2024-09-05 23:36:52 +0300

    reservation request counter

--- plugins/swordbros/booking/Plugin.php
+++ plugins/swordbros/booking/Plugin.php
@@ -1,6 +1,10 @@
 <?php namespace Swordbros\Booking;
 
+use Backend;
+use Swordbros\Booking\Controllers\BookingRequest;
+use Swordbros\Booking\models\BookingRequestModel;
 use System\Classes\PluginBase;
+use Yaml;
 
 /**
  * Plugin class
@@ -36,5 +40,41 @@ class Plugin extends PluginBase
      */
     public function registerSettings()
     {
+    }
+    public function registerNavigation()
+    {
+        $navigation = [
+            'main-menu-item' => [
+                'label'       => 'Booking',
+                'url'         => Backend::url('swordbros/booking/bookingrequest'),
+                'icon'        => 'icon-tags',
+                'order'=> 402,
+                'counter'      => BookingRequestModel::where(['is_new'=> 1])->count(),
+                'counterLabel' => 'New Booking Requests',
+                'sideMenu' => [
+                    'side-menu-item' => [
+                        'label' => 'Calendar',
+                        'url' => Backend::url('swordbros/booking/bookingcalendar'),
+                        'icon' => 'icon-calendar',
+                    ],
+                    'side-menu-item2' => [
+                        'label' => 'Requests',
+                        'url' => Backend::url('swordbros/booking/bookingrequest'),
+                        'icon' => 'icon-book',
+                    ],
+                    'side-menu-item3' => [
+                        'label' => 'Bookings',
+                        'url' => Backend::url('swordbros/booking/booking'),
+                        'icon' => 'icon-puzzle-piece',
+                    ],
+                    'side-menu-item4' => [
+                        'label' => 'Create Booking',
+                        'url' => Backend::url('swordbros/booking/booking/create'),
+                        'icon' => 'icon-tag',
+                    ],
+                ],
+            ],
+        ];
+        return $navigation;
     }
 }
--- plugins/swordbros/booking/models/bookingrequestmodel/_site_id.php
+++ plugins/swordbros/booking/models/bookingrequestmodel/_site_id.php
@@ -1,6 +1,10 @@
 <?php
     $site = \Site::getSiteFromId($record->site_id);
     if ($site) {
-        echo  '<span class="event-translate nav-icon nav-icon-flag" ><i class="'.$site->flagIcon.'"></i></span>'; // Dizi olarak döner
+        $indicator = '';
+        if($record->is_new){
+            $indicator = '<span class="status-indicator" style="background:#ff5e00"></span>'; // Dizi olarak döner
+        }
+        echo  '<span class="event-translate nav-icon nav-icon-flag" ><i class="'.$site->flagIcon.'"></i> '.$indicator.'</span>'; // Dizi olarak döner
     }
 
